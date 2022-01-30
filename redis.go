e0aa0ef2834ade9df0478b6d208454f9caf9dee4
Aleksandr Kalinkin <aleksandr@kalink.in>
2022-01-31 02:47:12 +0400

    Update

--- redis.go
+++ redis.go
@@ -2,13 +2,11 @@ package go_cache
 
 import (
 	"github.com/go-redis/redis"
-	"sync"
 	"time"
 )
 
 type redisCache struct {
 	client *redis.Client
-	keys   sync.Map
 }
 
 func NewRedis(addr, password string, db int) (*redisCache, error) {
@@ -25,15 +23,10 @@ func NewRedis(addr, password string, db int) (*redisCache, error) {
 
 	return &redisCache{
 		client: client,
-		keys:   sync.Map{},
 	}, nil
 }
 
 func (r *redisCache) Get(key string) (interface{}, error) {
-
-	if !r.keyExists(key) {
-		return nil, errNotFound
-	}
 
 	val, err := r.client.Get(key).Result()
 	if err != nil {
@@ -49,15 +42,11 @@ func (r *redisCache) Set(key string, val interface{}, duration time.Duration) er
 		return err
 	}
 
-	r.keyStore(key, duration)
-
 	return nil
 }
 
 func (r *redisCache) Delete(key string) error {
 	err := r.client.Del(key).Err()
-
-	r.keys.Delete(key)
 
 	return err
 }
@@ -65,32 +54,5 @@ func (r *redisCache) Delete(key string) error {
 func (r *redisCache) Flush() error {
 	err := r.client.FlushDB().Err()
 
-	r.keys = sync.Map{}
-
 	return err
-}
-
-func (r *redisCache) keyExists(key string) bool {
-
-	expiration, exists := r.keys.Load(key)
-	if !exists {
-		return false
-	}
-
-	if time.Now().After(expiration.(time.Time)) {
-		r.keys.Delete(key)
-		return false
-	}
-
-	return true
-}
-
-func (r *redisCache) keyStore(key string, duration time.Duration) {
-	var nowTime = time.Now()
-
-	if duration == 0 {
-		nowTime = nowTime.Add(1 << 16 * time.Hour)
-	}
-
-	r.keys.Store(key, nowTime.Add(duration))
 }
